0a78e775afda9324dfecc544c5261fb266e7b7f5
Nahima-ayni <nahimamadey1@gmail.com>
2024-07-11 11:13:44 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,7 +4,8 @@
 
 def minOperations(n):
     """
-    method that calculates the fewest number of operations needed to result in exactly n H characters in the file
+    method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file
     """
     if n < 2:
         return 0
